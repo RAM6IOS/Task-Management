5f389d80bb611e53e23c4ece2121cc01b3aee136
Bouchedoub Ramzi <ios191169@gmail.com>
2023-03-27 14:29:27 +0100

    first commit

---
+++ Task_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagementApp.swift
+//  Task Management
+//
+//  Created by Bouchedoub Ramzi on 27/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
